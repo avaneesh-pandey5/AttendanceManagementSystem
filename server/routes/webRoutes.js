cd0299361a8980f84bf3dd0d042d81b782410c3c
Aditya <adityasinghboss1234@gmail.com>
2023-02-23 02:41:49 +0530

    imported calendar route handler

--- server/routes/webRoutes.js
+++ server/routes/webRoutes.js
@@ -3,16 +3,19 @@ const {
   login,
   getStats,
   allStudents,
+  subjectinfo,
 } = require("../controllers/webControllers.js");
 const router = express.Router();
-const db = require("../database.js");
+
 const { isWebAuthenticated } = require("../middlewares/Auth.js");
 
 router.route("/login").post(login);
 
 router.route("/getStats/:subjectCode").get(isWebAuthenticated, getStats);
 
-router.route("/calender/:batch_id/:date/:enrollment_no").get(isWebAuthenticated, subjectinfo);
+router
+  .route("/calender/:batch_id/:date/:enrollment_no")
+  .get(isWebAuthenticated, subjectinfo);
 
 router.route("/demo").get(isWebAuthenticated, allStudents);
 
