ec68bb189bfe3f45f87a85628197551137b2f891
Chaitanya Rai <95732261+Chaitanyarai899@users.noreply.github.com>
2023-02-22 19:13:55 +0530

    Calendar  control functions added

--- server/controllers/webControllers.js
+++ server/controllers/webControllers.js
@@ -123,3 +123,82 @@ exports.allStudents = async (req, res) => {
     });
   }
 };
+
+
+// calendar controller functions.
+
+exports.getPeriodId = (date, batch_id) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = `SELECT period_id FROM period_id WHERE batch_ID = '${batch_id}' AND stamp LIKE '${date}%';`;
+      db.query(sql, (err, result) => {
+        if (err) {
+          console.error('Error fetching data from MySQL:', err);
+          reject({statusCode: 500, message: 'Internal Server Error'});
+        } else {
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      console.error('Error:', error);
+      reject({statusCode: 500, message: 'Internal Server Error'});
+    }
+  });
+};
+
+exports.getPA = (enrollment_no, date) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = `SELECT PA FROM attendance WHERE enrollment_no = '${enrollment_no}' AND PA LIKE '%${date}%';`;
+      db.query(sql, (err, result) => {
+        if (err) {
+          console.error('Error fetching data from MySQL:', err);
+          reject({statusCode: 500, message: 'Internal Server Error'});
+        } else {
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      console.error('Error:', error);
+      reject({statusCode: 500, message: 'Internal Server Error'});
+    }
+  });
+};
+
+
+exports.intersection = (period_id, PA) => {
+  const attended = period_id.filter(value => PA.includes(value));
+  return attended;
+};
+
+exports.uncommonitem = (period_id, PA) => {
+  const unattended = new Set(period_id.concat(PA).filter(v => !period_id.includes(v) || !PA.includes(v)));
+  return Array.from(unattended);
+};
+
+exports.periodID_to_Subjects = (attended) => {
+  const subjectcode = attended.map(value => value.slice(8, 14));
+  return subjectcode;
+};
+
+// only this function will be used by api
+
+exports.subjectinfo = async (date, enrollment_no, batch_id) => {
+  try {
+    const [period_id, PA] = await Promise.all([exports.getPeriodId(date, batch_id), exports.getPA(enrollment_no, date)]);
+
+    const Present = exports.periodID_to_Subjects(exports.intersection(period_id.map(value => value.period_id), PA.map(value_1 => value_1.PA)));
+
+    const notPresent = exports.periodID_to_Subjects(exports.uncommonitem(period_id.map(value_2 => value_2.period_id), PA.map(value_3 => value_3.PA)));
+
+    const Final = `Attended Subjects: ${Present}, Unattended Subjects: ${notPresent}`;
+
+    return Final;
+  
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
