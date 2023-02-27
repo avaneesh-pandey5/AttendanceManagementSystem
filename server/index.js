507f70523b62d90963a376b62255a536b34c9c2f
Aditya <adityasinghboss1234@gmail.com>
2023-02-27 20:58:50 +0530

    Allowing access for all origins

--- server/index.js
+++ server/index.js
@@ -8,11 +8,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 //Importing Routes
 const webRoutes = require("./routes/webRoutes");
